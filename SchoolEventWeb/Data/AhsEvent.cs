7181fedbd3a0a9b574cdfe129317de88de2385ca
TharunSaravanan <bstharun@gmail.com>
2023-01-19 12:07:15 -0600

    event data entry screen

--- SchoolEventWeb/Data/AhsEvent.cs
+++ SchoolEventWeb/Data/AhsEvent.cs
@@ -1,12 +1,21 @@
-﻿namespace SchoolEventWeb.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolEventWeb.Data
 {
     public class AhsEvent
     {
         public int Id { get; set; }
-        public string Name { get; set; }    
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Range(10, 50, ErrorMessage = "Attendance points should be between 10 and 50.")]
         public int AttendancePoints { get; set; }
+        [Range(25, 100, ErrorMessage = "Participation points should be between 10 and 50.")]
         public int ParticipationPoints { get; set; }
-        public DateTime Time { get; set; }
+        public DateTime Time { get; set; } = DateTime.Now;
     }
 }
