51de5a81a6635dcddfba5b15b582034d00311bdf
Tharun Saravanan <bstharun@gmail.com>
2023-01-21 10:03:57 -0600

    student page

--- SchoolEventWeb/Data/Student.cs
+++ SchoolEventWeb/Data/Student.cs
@@ -1,9 +1,14 @@
-﻿namespace SchoolEventWeb.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolEventWeb.Data
 {
     public class Student
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
-        public int Grade { get; set; }
+
+        [Range(9, 12, ErrorMessage = "Grade should be between 9 to 12")]
+        public int Grade { get; set; } = 9;
     }
 }
