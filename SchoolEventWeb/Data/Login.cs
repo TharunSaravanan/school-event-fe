0200d5461d7d4645f9ad95f93bd8e759cbf041f7
Tharun Saravanan <bstharun@gmail.com>
2023-05-24 23:17:25 -0500

    login page pt1

---
+++ SchoolEventWeb/Data/Login.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolEventWeb.Data
+{
+	public class User
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public int Email { get; set; }
+
+		[Required]
+		public int Password { get; set; }
+
+		[Required]
+		public int Type { get; set; }
+
+		public User()
+		{
+		}
+	}
+}
+
